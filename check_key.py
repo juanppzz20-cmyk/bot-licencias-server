7585ec55bc85d5ea4b3c2e78f4039ec495236537
juanppzz20-cmyk <juanppzz20@gmail.com>
2026-07-05 16:43:39 -0200

    Update check_key.py

--- check_key.py
+++ check_key.py
@@ -51,4 +51,4 @@ def clave_de_verificacion():
     return jsonify({"estado":"error","mensaje":"KEY invalida"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=10000)
